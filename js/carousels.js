741d44166708c64d6dae36ca14ca725a43fcedbf
LeeChangHwan <changhwanlee94@gmail.com>
2022-04-01 19:16:01 +0900

    🚧 Separate .user-gallery: mobile / desktop

--- js/carousels.js
+++ js/carousels.js
@@ -11,22 +11,28 @@ const productCarousel = tns({
   preventScrollOnTouch: true,
 })
 
-const userGallery = tns({
-  container: '.user-gallery-slider .slider-list',
+const userGalleryMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
   controls: false,
-  controlsContainer: '.user-gallery-controls',
-  navContainer: '.user-gallery-thumbnail .thumbnail-list',
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
   gutter: 4,
   edgePadding: 16,
   loop: false,
   arrowKeys: true,
   mouseDrag: true,
   preventScrollOnTouch: true,
-  responsive: {
-    768: {
-      gutter: 6,
-      controls: true,
-      edgePadding: 75,
-    },
-  },
+})
+
+const userGalleryDesktop = tns({
+  container: '.user-gallery.is-desktop .slider-list',
+  controls: true,
+  controlsContainer: '.user-gallery.is-desktop .user-gallery-controls',
+  navContainer:
+    '.user-gallery.is-desktop .user-gallery-thumbnail .thumbnail-list',
+  gutter: 6,
+  edgePadding: 75,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
 })
