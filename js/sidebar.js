f38588dc261ac17ac4803c027889024e51d2daa2
LeeChangHwan <changhwanlee94@gmail.com>
2022-04-03 16:13:40 +0900

    ✨ Handle open & close  sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,15 @@
+const menuButton = document.querySelector('.gnb-icon-button.is-menu')
+const sidebar = document.querySelector('.sidebar')
+const sidebarOverlay = document.querySelector('.overlay')
+
+const openSidebar = () => {
+  sidebar.classList.add('is-active')
+  sidebarOverlay.classList.add('is-active')
+}
+menuButton.addEventListener('click', openSidebar)
+
+const closeSidebar = () => {
+  sidebar.classList.remove('is-active')
+  sidebarOverlay.classList.remove('is-active')
+}
+sidebarOverlay.addEventListener('click', closeSidebar)
