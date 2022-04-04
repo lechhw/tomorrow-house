fb175227c5984fba81ae0ccc52b864d17b058422
LeeChangHwan <changhwanlee94@gmail.com>
2022-04-04 17:35:44 +0900

    🚧 Implemenet delete all .gnb-search-history items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -2,7 +2,7 @@ const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
 
-const DeleteAllButton = gnbSearchHistory.querySelector(
+const deleteAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
 const searchHistoryList = gnbSearchHistory.querySelector('ol')
@@ -15,9 +15,19 @@ function closeGnbSearchHistory(e) {
 }
 
 function openGnbSearchHistory() {
+  if (searchHistoryList.children.length == 0) {
+    return
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory)
   }
   gnbSearchHistory.classList.add('is-active')
 }
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function deleteAllHistoryItems() {
+  searchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+deleteAllButton.addEventListener('click', deleteAllHistoryItems)
