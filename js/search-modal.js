0fc8ed65aaf3e3379d5e84249a9f79096cecbac5
LeeChangHwan <changhwanlee94@gmail.com>
2022-04-05 17:03:59 +0900

    Updates

--- js/search-modal.js
+++ js/search-modal.js
@@ -3,14 +3,36 @@ const searchModal = document.querySelector('.search-modal')
 const searchModalCloseButton = searchModal.querySelector('.btn-ghost.btn-40')
 const searchModalOverlay = document.querySelector('.overlay')
 
+const historyList = searchModal.querySelector('ol')
+const historyDeleteAllButton = searchModal.querySelector(
+  '.search-history-header button'
+)
+const historyDeleteButtonList = searchModal.querySelectorAll(
+  '.search-history-item .delete-button'
+)
+
 const openSearchModal = () => {
   searchModal.classList.add('is-active')
   searchModalOverlay.classList.add('is-active')
 }
-searchButton.addEventListener('click', openSearchModal)
 
 const closeSearchModal = () => {
   searchModal.classList.remove('is-active')
   searchModalOverlay.classList.remove('is-active')
 }
+
+function deleteAllHistoryItems() {
+  historyList.innerHTML = ''
+}
+
+function deleteHistoryItem() {
+  const historyItem = this.parentNode
+  historyList.removeChild(historyItem)
+}
+
+historyDeleteButtonList.forEach((button) => {
+  button.addEventListener('click', deleteHistoryItem)
+})
+historyDeleteAllButton.addEventListener('click', deleteAllHistoryItems)
 searchModalCloseButton.addEventListener('click', closeSearchModal)
+searchButton.addEventListener('click', openSearchModal)
